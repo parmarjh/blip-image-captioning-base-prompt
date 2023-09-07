36f0973e62aa1707473334cca0d6d7b659aa5928
Jatinkumar Parmar <62230070+parmarjh@users.noreply.github.com>
2023-09-07 12:33:45 +0530

    Add files via upload

---
+++ gradio_ui.py
@@ -0,0 +1,11 @@
+import gradio as gr
+
+gr.Interface.load("models/Salesforce/blip-image-captioning-base").launch()
+
+iface = gr.Interface(
+    fn=generate_caption,
+    inputs=gr.inputs.Image(label="Upload an image"),
+    outputs=gr.outputs.Textbox(label="Generated Caption"),
+)
+
+iface.launch(share=True)
